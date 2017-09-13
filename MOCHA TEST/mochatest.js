bb9af1d37ba56c6df9615373e941c68963f5ea09
wallytreats <nicklawrenceemail@gmail.com>
2017-09-13 13:30:02 -0600

    wrote some tests

---
+++ MOCHA TEST/mochatest.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const assert = chai.assert;
+const vowels = require('../vowels');
+const { countVowels } = vowels
+
+describe('vowels', function(){
+  it('returns 0 if there are no vowels in the given string', function(){
+    assert.equal(countVowels('str'), 0)
+  });
+  it('returns the number of vowels in a given string', function(){
+    assert.equal(countVowels('hello world'), 3)
+  });
+  it('only takes strings as args', function(){
+    assert.equal(countVowels([1,2,3,4]), 'please enter a string')
+    assert.equal(countVowels(), 'please enter a string')
+  })
+})
